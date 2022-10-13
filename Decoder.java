0d047054a01205b2e38096a47c3027ee3cc2e6aa
Svetlana Saltykova <libanonair@gmail.com>
2022-10-14 00:46:09 +0300

    add russian symbols to encoder

--- Decoder.java
+++ Decoder.java
@@ -8,28 +8,11 @@ public class Decoder {
     public Decoder() {
     }
 
-    Dictionary dic = new Dictionary();
     Encoder encoder = new Encoder();
 
     public String decodeText(String text, int key) {
         key = -key;
-        StringBuilder builder = new StringBuilder(text);
-
-        for (int i = 0; i < builder.length(); i++) {
-            char element = builder.charAt(i);
-            if (element >= 'a' && element <= 'z') {
-                int indexOfAlphabet = dic.ALPHABET.indexOf(element);
-                Collections.rotate(dic.ALPHABET,key);
-                builder.setCharAt(i, dic.ALPHABET.get(indexOfAlphabet));
-
-            } else if (element >= 'A' && element <= 'Z') {
-                int indexOfUpperAlphabet = dic.ALPHABETUPPERCASE.indexOf(element);
-                Collections.rotate(dic.ALPHABETUPPERCASE,key);
-                builder.setCharAt(i, dic.ALPHABETUPPERCASE.get(indexOfUpperAlphabet));
-            }
-        }
-
-        return builder.toString();
+        return  encoder.encodeText(text,key);
     }
 
     public Path getNewFileNameForDecoded(Path path) {
