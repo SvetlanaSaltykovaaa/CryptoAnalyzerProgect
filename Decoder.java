0d6755194c45089af2bd8cd949ecb7b6f694687d
Svetlana Saltykova <libanonair@gmail.com>
2022-10-14 14:13:52 +0300

    delete line in decodeText method

--- Decoder.java
+++ Decoder.java
@@ -1,6 +1,9 @@
 package javarush.project;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 
 public class Decoder {
@@ -11,8 +14,7 @@ public class Decoder {
     Encoder encoder = new Encoder();
 
     public String decodeText(String text, int key) {
-        key = -key;
-        return  encoder.encodeText(text,key);
+        return  encoder.encodeText(text,- key);
     }
 
     public Path getNewFileNameForDecoded(Path path) {
@@ -22,5 +24,13 @@ public class Decoder {
         path = Path.of(str);
 
         return path;
+    }
+
+    public void writeToFile(Path filePath,String decodedText){
+        try {
+            Files.writeString(getNewFileNameForDecoded(filePath), decodedText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
